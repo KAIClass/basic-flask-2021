cb8cb637e4d9ddbc8c168db3751ebdb004304404
kushalshm1 <sharmakushal2k@gmail.com>
2021-01-21 22:38:03 +0530

    regression class added

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+
 class company:
     def __init__(self,price):
         self.share_price = price
@@ -10,15 +12,29 @@ class company:
     def trade(self,cost,misc,sell):
         self.ret = sell - (cost + misc)
         if(ret<0):
-            # people shall sell shares if company has negative performance
-            # you have to connect this with time 
+            self.sell(10)
+        elif(ret>0):
+            self.sell(10)
         return ret
 
-        
-msft = company(100)
-for i in range(1000):
-    if(random.random() < 0.3):
-        msft.sell(0)
-    else:
-        msft.buy(0)
-print(msft.share_price)
+class regression:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.m = [0]*len(x.T)
+        self.c = 2
+        self.line_mag = self.m*self.x + self.c
+        self.y_p = self.line_mag
+        self.error_mag = self.line_mag - self.y_p
+    def error(self):
+        return np.mean((self.line_mag-self.y_a))**2
+    def gradient_m(self):
+        return 2*np.mean(self.error_mag)*x
+    def gradient_c(self):
+        return 2*np.mean(self.error_mag)
+    def fit(self,iters):
+        for i in range(iters):
+            self.m -= self.gradient_m()
+            self.c -= self.gradient_c() 
+    def predict(self,test_x):
+        return (self.m * test_x) + self.c
